9ee8473b489a3364fdc46f6a3fde1683229dda52
PrincyM <prinna.pm@gmail.com>
2018-06-28 14:32:17 -0400

    added Movie model

---
+++ src/app/movie.ts
@@ -0,0 +1,11 @@
+export class Movie {
+    id: number;
+ title: string;
+ description: string;
+ genre: string;
+ duration: string;
+ cast: string;
+ directors: string;
+ release_year: string;
+ image_url: string;
+}
