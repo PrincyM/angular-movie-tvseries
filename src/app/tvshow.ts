76037e44b526e0aa96f61efda11e9dba5d1a6995
Elizabeth <lizareji23@gmail.com>
2018-06-28 11:03:42 -0400

    Added model TVShow

---
+++ src/app/tvshow.ts
@@ -0,0 +1,11 @@
+export class Tvshow {
+    id: string;
+    name: string;
+    seasonNumber: number;
+    episodeNumber: number;
+    description: string;
+    cast: string;
+    creators: string;
+    imageUrl: string;
+    genre: string;
+}
